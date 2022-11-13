4b724511ea3328ba21a2c7ec5e0f80c864a4c0f8
Thomas Duckworth <98187423+tduck96@users.noreply.github.com>
2022-11-13 13:42:26 -0600

    updates to club and cookie options

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -230,9 +230,8 @@ namespace RealPetApi.Services
                 IsEssential = true,
                 HttpOnly = true,
                 Expires = refreshToken.Expires,
-                //Secure = true,
-                Secure = false,
-                //SameSite = SameSiteMode.None
+                Secure = true,
+                SameSite = SameSiteMode.None
 
 
             };
@@ -255,9 +254,8 @@ namespace RealPetApi.Services
                 Path = "/",
                 IsEssential = true,
                 HttpOnly = true,
-                //Secure = true,
-                Secure = false,
-                //SameSite = SameSiteMode.None
+                Secure = true,
+                SameSite = SameSiteMode.None
             };
 
             _httpContextAccessor?.HttpContext?.Response
